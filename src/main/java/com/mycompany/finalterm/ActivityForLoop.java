20757b033732a6a8a678fa702527b7648b3c56bd
DESKTOP-RQ7TC78\User <salengamo@students.nu-clark.edu.ph>
2024-10-10 10:58:00 +0800

    [;plokijuhygnm,;'/.

--- src/main/java/com/mycompany/finalterm/ActivityForLoop.java
+++ src/main/java/com/mycompany/finalterm/ActivityForLoop.java
@@ -1,3 +1,5 @@
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -15,6 +17,7 @@ public class ActivityForLoop {
         double[] grade = new double[5];
         String[] subjects = {"Math", "English", "History", "Science", "Programming"};
         
+        //INPUT GRADE
         System.out.println("Please enter the grades of the subjects: ");
         double grades; 
         for (int i = 0; i < subjects.length; i++){
@@ -25,7 +28,7 @@ public class ActivityForLoop {
             System.out.println("Invalid input please enter grade " + subjects[i]);
             scanner.next();
             } 
-            
+           // GRADES SHOULD BE 65 TO 100
         grades = scanner.nextDouble();
         if (grades < 65 || grades > 100) {
              System.out.println("Invalid grade please enter a value from 65 - 100");
@@ -44,7 +47,7 @@ public class ActivityForLoop {
         for (int i = 0; i < subjects.length; i++) {
             System.out.println(subjects[i] + " : " + grade[i]);
         }
-        
+        // GET THE AVERAGE
          System.out.println("total no. of Subject: " + grade.length);
             int totalSum = 0;
             for (int i = 0; i < grade.length; i++) {
@@ -54,7 +57,8 @@ public class ActivityForLoop {
             double average = (double)totalSum / grade.length;
             System.out.printf("Total Average: %.2f", average);
             
-        System.out.println("Grade Classification: ");
+            // GRADE CLASSIFICATION
+        System.out.println("\nGrade Classification: ");
         for (int i = 0; i < grade.length; i++) {
             char gradesClassification;
             if (grade[i] >= 90) {
@@ -69,6 +73,42 @@ public class ActivityForLoop {
         System.out.println(subjects[i] + ":" + grade[i] + "-> " + gradesClassification);
        
         }
+        
+        // CHECK THE SUBJECT BELOW 75
+        System.out.println("Subjects with grades below 75");
+        for (int i = 0; i < subjects.length; i++) {
+            if (grade[i] < 75){
+                 System.out.print("\n" + subjects[i] + " : " + grade[i]);
+            }  
+            // "\n%.2f : .%2f", subjects[i],grade[i]
+            // "\n" + subjects[i] + " : " + grade[i]
+        }
+        
+        double maxGrade = grade[0];
+        double minGrade = grade[0];
+        String maxSubjects = subjects[0];
+        String minSubjects = subjects[0];
+        
+        for (int i = 1; 1 < grade.length; i++) {
+            if (grade[i] > maxGrade) {
+                maxGrade = grade[i];
+                maxSubjects = subjects[i];
+            }
+        }
+        
+        System.out.println("\nHighest grade: " + maxSubjects + " : " + maxGrade);
+
+        
+         
+        for (int i = 1; i > grade.length; i++) {
+            if (grade[i] < minGrade) {
+                minGrade = grade[i];
+                minSubjects = subjects[i];
+            }
+        }
+        
+        System.out.println("\nLowest grade: " + minSubjects + " : " + minGrade);
+
         scanner.close();
     }
 }
